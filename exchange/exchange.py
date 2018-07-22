72ae20c53f4773ddd7329524e4d7f7735bdc2682
KadekYuda <yudaikadek22@gmail.com>
2018-07-22 11:21:38 +0700

    initial commit

--- exchange/exchange.py
+++ exchange/exchange.py
@@ -6,9 +6,9 @@ def convertecurrencyrates(base, to, amount=1):
     currencyRates = CurrencyRates(force_decimal=True)
     try:
         rates = currencyRates.convert(base, to, Decimal(amount))
-        print(str(amount) + " " + base + " = " + str(rates) + " " + to)
+        return str(amount) + " " + base + " = " + str(rates) + " " + to
     except RatesNotAvailableError as e:
-        print("Error! the rates data are not available.")
+        return "Error! the rates data are not available."
 
 #
 # if __name__ == "__main__":
